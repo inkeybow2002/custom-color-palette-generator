78ec8cf20d5777c818b1dbc6aa8fdf76aafe760e
inkeybow2002 <ogs3rvl161k6@hotmail.com>
2025-08-31 10:29:37 +0100

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
+function App() {
+  return (
+    <Container>
+      <Typography variant="h1">Custom Color Palette Generator</Typography>
+      {/* Add your components here */}
+    </Container>
+  );
+}
+
+export default App;
